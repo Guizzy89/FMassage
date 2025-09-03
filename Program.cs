151ce05f8455aedcaf4cc0ae0254eb0f3fa49a98
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-09-03 19:01:42 +0300

    Ещё установил пакет NuGet dotnet add package Microsoft.AspNetCore.Identity.UI

--- Program.cs
+++ Program.cs
@@ -1,5 +1,39 @@
+using FMassage.Data;
+using FMassage.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Добавляем поддержку базы данных SQLite
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=FMassage.db"));
+
+// Регистрация поддержки Asp.Net Core Identity
+builder.Services.AddDefaultIdentity<User>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts(); // Включаем HTTPS Strict Transport Security Policy
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 
